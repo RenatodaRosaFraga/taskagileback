f185e79158ad9b272b29c51cd0fe2fb4950bfc64
Renato <renato.fraga@alunos.sc.senac.br>
2026-04-16 22:02:21 -0300

    aula 1604

--- src/main/java/com/senac/taskagile/taskagileback/controllers/AuthController.java
+++ src/main/java/com/senac/taskagile/taskagileback/controllers/AuthController.java
@@ -4,6 +4,7 @@ package com.senac.taskagile.taskagileback.controllers;
 import com.senac.taskagile.taskagileback.model.DTO.LoginRequest;
 import com.senac.taskagile.taskagileback.model.DTO.LoginResponse;
 import com.senac.taskagile.taskagileback.model.repository.UsuarioRepository;
+import com.senac.taskagile.taskagileback.services.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.Table;
@@ -20,13 +21,20 @@ public class AuthController {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping("/login")
     @Operation(description = "Valida senha asdasad 50 caracteres, calcula a longitude", summary = "Login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest){
 
 
         if(loginRequest.email().equals("String@s") && loginRequest.senha().equals("String@s")){
-            return ResponseEntity.ok(new LoginResponse("Sskjaks123"));
+
+            var token = tokenService.gerarToken(loginRequest.email());
+
+
+            return ResponseEntity.ok(new LoginResponse(token));
         }
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
